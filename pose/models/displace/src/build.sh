b593fba892fdd9907825b86ebb2d6e3612eb4e54
texot <tete1030@gmail.com>
2018-08-30 20:51:44 +0800

    Fix torch extension building problem

--- pose/models/displace/src/build.sh
+++ pose/models/displace/src/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export CC=g++-5
+export CXX=g++-5
 python setup.py build --build-lib ./build
 shopt -s extglob
 cp "./build/displace_cuda."@(*)".so" ../displace_cuda.so
--- pose/models/lacorr2d/src/build.sh
+++ pose/models/lacorr2d/src/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export CC=g++-5
+export CXX=g++-5
 python setup.py build --build-lib ./build
 shopt -s extglob
 cp "./build/lacorr2d_cuda."@(*)".so" ../lacorr2d_cuda.so
